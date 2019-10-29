0489c557430f4b7d122f97e230998eae42e54acd
Karla Guzmán <karla_g97@hotmail.com>
2019-10-29 16:57:56 -0600

    Componentes y servicios
    Se añade el componente y servicio 'clasificador'.

---
+++ src/app/clasificador/clasificador.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClasificadorComponent } from './clasificador.component';
+
+describe('ClasificadorComponent', () => {
+  let component: ClasificadorComponent;
+  let fixture: ComponentFixture<ClasificadorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ClasificadorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClasificadorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
